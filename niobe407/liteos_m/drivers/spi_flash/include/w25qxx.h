a197b1abb1c737ef9a8cad3e5b1dd6771e308a56
拓维信息候鹏飞 <houpengfei@talkweb.com.cn>
2022-04-25 21:28:40 +0800

    niobe407 告警清零
    Signed-off-by: 拓维信息候鹏飞 <houpengfei@talkweb.com.cn>
    Change-Id: I84aaa60df24543d140a4c45addef7209a592c2aa

--- niobe407/liteos_m/drivers/spi_flash/include/w25qxx.h
+++ niobe407/liteos_m/drivers/spi_flash/include/w25qxx.h
@@ -20,16 +20,16 @@
 #include "hdf_log.h"
 #include "spi_if.h"
 
-#define W25x_ID                         0X4018     // W25Q128  16MB 
+#define W25x_ID 0X4018 // W25Q128  16MB
 
 uint8_t W25x_InitSpiFlash(uint32_t busNum, uint32_t csNum);
 uint8_t W25x_DeInitSpiFlash(void);
 DevHandle W25x_GetSpiHandle(void);
 void W25x_SectorErase(uint32_t SectorAddr);
 void W25x_BulkErase(void);
-void W25x_PageWrite(uint8_t* pBuffer, uint32_t WriteAddr, uint16_t NumByteToWrite);
-void W25x_BufferWrite(uint8_t* pBuffer, uint32_t WriteAddr, uint16_t NumByteToWrite);
-void W25x_BufferRead(uint8_t* pBuffer, uint32_t ReadAddr, uint16_t NumByteToRead);
+void W25x_PageWrite(uint8_t *pBuffer, uint32_t WriteAddr, uint16_t NumByteToWrite);
+void W25x_BufferWrite(uint8_t *pBuffer, uint32_t WriteAddr, uint16_t NumByteToWrite);
+void W25x_BufferRead(uint8_t *pBuffer, uint32_t ReadAddr, uint16_t NumByteToRead);
 uint32_t W25x_ReadID(void);
 uint32_t W25x_ReadDeviceID(void);
 void W25x_StartReadSequence(uint32_t ReadAddr);
@@ -42,4 +42,4 @@ void W25x_WriteEnable(void);
 void W25x_WaitForWriteEnd(void);
 #endif
 
-#endif  /* __BSP_SPIFLASH_H__ */
+#endif /* __BSP_SPIFLASH_H__ */
